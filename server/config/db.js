4dfdddb493b92a3a30508be35fce89f28404c56f
Li jiamei <jli479@my.centennialcollege.ca>
2022-11-03 22:26:22 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://JiameiLi:RiceMonkey2828@cluster0.gfonero.mongodb.net/?retryWrites=true&w=majority"
+};
---
+++ server/routes/books.js
@@ -0,0 +1,122 @@
+//Jiamei Li
+
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the book model
+let book = require('../models/books');
+
+/* GET books List page. READ */
+router.get('/', (req, res, next) => {
+  // find all books in the books collection
+  book.find( (err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/index', {
+        title: 'Books',
+        books: books
+      });
+    }
+  });
+
+});
+
+//  GET the Book Details page in order to add a new Book
+router.get('/add', (req, res, next) => {
+
+    res.render('books/details', {title: 'Add a Book', books: {}});
+
+});
+
+// POST process the Book Details page and create a new Book - CREATE
+router.post('/add', (req, res, next) => {
+
+    let newBook = book({
+      Title: req.body.title,
+      Description: req.body.description,
+      Price: req.body.price,
+      Author: req.body.author,
+      Genre: req.body.genre
+    })
+
+    console.log(newBook);
+
+    book.create(newBook, (err, book) => {
+      if(err){
+        console(err);
+        res.end(err);
+      }
+      else{
+        res.redirect('/books');
+      }
+    })
+
+});
+
+// GET the Book Details page in order to edit an existing Book
+router.get('/:id', (req, res, next) => {
+
+    console.log(req.params.id);
+
+    book.findById({_id : req.params.id}, (err, book) => {
+      if(err){
+        console.log(err);
+        res.end(err);
+      }
+      else{
+        res.render('books/details', {title: 'Edit a Book', books: book});
+      }
+    })
+
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/:id', (req, res, next) => {
+
+    console.log(req.params.id);
+
+    let id = req.params.id;
+
+    let updatedBook = book({
+      _id : id,
+      Title: req.body.title,
+      Description: req.body.description,
+      Price: req.body.price,
+      Author: req.body.author,
+      Genre: req.body.genre
+    });
+
+    book.update({_id : id}, updatedBook, (err, book) => {
+      if(err){
+        console.log(err);
+        res.end(err);
+      }
+      else{
+        res.redirect('/books');
+      }
+    });
+
+});
+
+// GET - process the delete by user id
+router.get('/delete/:id', (req, res, next) => {
+
+    console.log(req.params.id);
+
+    book.remove({_id : req.params.id}, (err) => {
+      if(err){
+        console.log(err);
+        res.end(err);
+      }
+      else{
+        res.redirect('/books');
+      }
+    })
+});
+
+
+module.exports = router;
